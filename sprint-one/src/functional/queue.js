23d2d3119883f91a176c4df8121c93340a621937
Joyce Ma <pikajoyce@Joyces-MacBook-Air.local>
2019-04-10 16:01:33 -0700

    Added functional classes implementation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,33 @@
 var Queue = function() {
   var someInstance = {};
-
+  var count = 0
+  var head = 0
+  var tail = 0
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    count += 1
+    storage[tail++] = value
   };
 
   someInstance.dequeue = function() {
+    var value = storage[head]
+    delete storage[head]
+    if (head < tail) {
+      head+=1
+    }
+    count -= 1
+    return value
   };
 
   someInstance.size = function() {
+    if (count <= 0) {
+      return 0
+    }
+    return count
   };
-
   return someInstance;
 };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,28 @@
 var Stack = function() {
   var someInstance = {};
-
+  var count = 0
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    count+= 1
+    storage[count] = value
+
   };
 
   someInstance.pop = function() {
+    var value = storage[count]
+    delete storage[count]
+    count -= 1
+    return value
   };
 
   someInstance.size = function() {
+    if (count <= 0) {
+      return 0
+    }
+    return count
   };
 
   return someInstance;
