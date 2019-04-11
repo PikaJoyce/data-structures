a21697dd9b8a09f0783afa4f07f182ff311f7298
Joyce Ma <pikajoyce@Joyces-MacBook-Air.local>
2019-04-10 21:09:52 -0700

    Added functional shared implementation

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,40 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {}
+  stack.storage = {}
+  stack.count = 0
+  stack.var = stack.storage[stack.count]
+  extend(stack, stackMethods);
+  return stack
 };
 
-var stackMethods = {};
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+var stackMethods = {
+
+	push(value) {
+		this.count++
+		this.storage[this.count] = value 
+	},
+
+	pop() {
+		this.var = this.storage[this.count]
+		delete this.storage[this.count]
+		this.count--
+		if (this.count < 0) {
+			this.count = 0
+		}
+		return this.var
+	},
+
+	size() {
+		return this.count
+	}
+};
 
 
