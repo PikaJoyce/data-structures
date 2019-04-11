641092a742e6ef982e568d3275a04f415dd2b72c
Joyce Ma <pikajoyce@Joyces-MacBook-Air.local>
2019-04-10 22:36:50 -0700

    Implemented ES6 instantiation pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,31 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.count = 0
+  	this.head = 0
+  	this.tail = 0
+  	this.storage = {}
   }
 
+  enqueue(value) {
+  	this.count++
+  	this.storage[this.tail++] = value
+  }
+
+  dequeue() {
+  	var returned = this.storage[this.head]
+  	delete this.storage[this.head]
+  	if(this.head < this.tail) {
+  		this.head++
+  	}
+  	this.count--
+  	return returned
+  }
+
+  size() {
+  	if(this.count < 0) {
+  		this.count = 0
+  	}
+  	return this.count
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.storage = {}
+  	this.count = 0
+  }
+
+  push(value){
+  	this.count++
+  	this.storage[this.count] = value
+  } 
+
+  pop() {
+  	var returned = this.storage[this.count]
+  	delete this.storage[this.count]
+  	this.count--
+  	return returned
+  }
+
+  size(){
+  	if(this.count < 0) {
+  		this.count = 0
+  	}
+  	return this.count
   }
 
 }
