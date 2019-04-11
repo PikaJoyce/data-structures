b546956dd5a26f70cc9d85b23cc4e2f22658002d
Joyce Ma <pikajoyce@Joyces-MacBook-Air.local>
2019-04-10 22:10:14 -0700

    Implemented prototypal instantiation pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods)
+  instance.count = 0
+  instance.head = 0
+  instance.tail = 0
+  instance.storage = {}
+  return instance
 };
 
-var queueMethods = {};
+var queueMethods = {
+	enqueue(value) {
+		this.count++
+		this.storage[this.tail++] = value
+	},
+	dequeue(){
+		var returned = this.storage[this.head]
+		delete this.storage[this.head]
+		if(this.head < this.tail) {
+			this.head++
+		}
+		this.count--
+		return returned
+	},
+	size(){
+		if (this.count < 0) {
+			this.count = 0
+		}
+		return this.count
+	}
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods)
+  instance.count = 0
+  instance.storage = {}
+  return instance
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push(value){
+		this.count++
+		this.storage[this.count] = value
+	},
+	pop(){
+		var returned = this.storage[this.count]
+		delete this.storage[this.count]
+		this.count--
+		return returned
+	},
+	size(){
+		if (this.count < 0) {
+			this.count = 0
+		}
+		return this.count
+	}
+
+};
 
 
